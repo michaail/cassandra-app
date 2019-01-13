192d21a7151967503106c746a8042e051d785aff
michaail <galeo09@gmail.com>
2019-01-13 18:20:51 +0100

    initial tests for partition

--- src/Runner.cs
+++ src/Runner.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using cassandra_app.src;
 using cassandra_app.src.Models;
 
@@ -21,7 +24,7 @@ namespace cassandra_app.src
 
         public Runner()
         {
-            backend = new BackendSession(false);
+            backend = new BackendSession(true);
 
             counterController = new Controllers.Tickets_Counter(backend);
             reservationController = new Controllers.Reservation(backend);
@@ -29,15 +32,31 @@ namespace cassandra_app.src
 
             List<Event> events = eventController.GetAllEvents();
 
-            Event ev = events.Where(e => e.Id == 2).First();
+            Event ev = events.Where(e => e.Id == 3).First();
 
             Console.WriteLine(ev.Total_Tickets);
-            for (int i=0; i<ev.Total_Tickets; i++)
-            {
-                Reservation res = new Reservation(Guid.NewGuid(), ev.Id, 10, 1, false, DateTimeOffset.Now);
-                reservationController.PlaceReservation(res);
-            }
+            Stopwatch sW = Stopwatch.StartNew();
 
+            List<Task> tasks = new List<Task>();
+            for (int j=0; j<5; j++)
+            {
+                Task task = Task.Factory.StartNew(() => run(36800));
+                tasks.Add(task);
+
+            }
+            Task.WaitAll(tasks.ToArray());
+            sW.Stop();
+            Console.WriteLine(sW.ElapsedMilliseconds);
+
+            long? value = counterController.GetRemainingTicketsValue(ev.Id);
+            Console.WriteLine(value);
+            
+            // for (int i=0; i<ev.Total_Tickets; i++)
+            // {
+            //     Reservation res = new Reservation(Guid.NewGuid(), ev.Id, 10, 1, false, DateTimeOffset.Now);
+            //     reservationController.PlaceReservation(res);
+            // }
+        
         }
 
         Event GetRandomEvent(List<Models.Event> events)
@@ -46,6 +65,15 @@ namespace cassandra_app.src
             // Event randomEvent;
             int r = random.Next(0, events.Count);
             return events[r];
+        }
+
+        public void run(int count)
+        {
+            for (int i=0; i<count; i++)
+            {
+                Reservation res = new Reservation(Guid.NewGuid(), 3, 10, 1, false, DateTimeOffset.Now);
+                reservationController.PlaceReservation(res);
+            }
         }
 
     }
